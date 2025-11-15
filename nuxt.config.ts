00468e1148ccc243d39a1bd6389ec2478b933e00
Owen Hathaway <owen@prsdata.com>
2025-11-14 20:13:13 -0700

    removed cloudflare-pages that Claude had added in

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -53,9 +53,9 @@ export default defineNuxtConfig({
   // Nitro config
   nitro: {
     experimental: {
-      tasks: true
-    },
-    preset: 'cloudflare-pages'
+      tasks: true,
+      openAPI: true
+    }
   },
 
   // Disable auto-import of password functions from nuxt-auth-utils
