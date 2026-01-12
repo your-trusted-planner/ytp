0f366201cabc2c6711f97200becae26296fd48ec
Owen Hathaway <owen@prsdata.com>
2026-01-11 20:14:53 -0700

    troubleshooting document template deletes

--- server/api/templates/[id].delete.ts
+++ server/api/templates/[id].delete.ts
@@ -1,14 +1,29 @@
 // Delete a template (soft or hard delete)
-// INCREMENTAL DEBUG v2: Add auth check
+// INCREMENTAL DEBUG v3: Add database query
 
 export default defineEventHandler(async (event) => {
   const { user } = await requireUserSession(event)
   const id = getRouterParam(event, 'id')
 
+  if (!id) {
+    throw createError({ statusCode: 400, message: 'Template ID required' })
+  }
+
+  const { useDrizzle, schema } = await import('../../db')
+  const { eq } = await import('drizzle-orm')
+  const db = useDrizzle()
+
+  const template = await db.select()
+    .from(schema.documentTemplates)
+    .where(eq(schema.documentTemplates.id, id))
+    .get()
+
   return {
     success: true,
-    debug: 'v2-auth',
+    debug: 'v3-db-query',
     userId: user?.id,
-    templateId: id
+    templateId: id,
+    templateFound: !!template,
+    templateName: template?.name
   }
 })
